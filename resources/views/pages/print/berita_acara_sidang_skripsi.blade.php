72902cfada5c6f49fae3b3f3aadc95c1591d8977
Mufti Restu Mahesa <mufti.restumahesa@gmail.com>
2022-06-21 22:05:43 +0700

    update view

--- resources/views/pages/print/berita_acara_sidang_skripsi.blade.php
+++ resources/views/pages/print/berita_acara_sidang_skripsi.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Cetak Berita Acara Siang Skripsi</h1>
+    <h1 class="h3 mb-0 text-gray-800">Cetak Berita Acara Sidang Skripsi</h1>
     <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
         <li class="breadcrumb-item active" aria-current="page">Cetak Berita Acara Sidang Skripsi</li>
